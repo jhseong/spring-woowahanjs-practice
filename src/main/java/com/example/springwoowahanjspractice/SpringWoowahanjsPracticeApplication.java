06103f6a2e3bb36203e7efd832c7cba2e5daa557
Estel Seong <estel.castle@gmail.com>
2017-10-09 09:47:46 +0900

    first commit

---
+++ src/main/java/com/example/springwoowahanjspractice/SpringWoowahanjsPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.example.springwoowahanjspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWoowahanjsPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWoowahanjsPracticeApplication.class, args);
+	}
+}
