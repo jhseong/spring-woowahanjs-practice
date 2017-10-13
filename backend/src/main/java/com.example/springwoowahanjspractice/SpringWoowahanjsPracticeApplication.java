aea7409e8e79bad992160c44a8967db36408ecc5
jhseong <jhseong@woowahan.com>
2017-10-13 13:56:11 +0900

    AWS Tomcat에서 서비스될 수 있도록 설정하기.

--- backend/src/main/java/com.example/springwoowahanjspractice/SpringWoowahanjsPracticeApplication.java
+++ backend/src/main/java/com.example/springwoowahanjspractice/SpringWoowahanjsPracticeApplication.java
@@ -2,11 +2,18 @@ package com.example.springwoowahanjspractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringWoowahanjsPracticeApplication {
+public class SpringWoowahanjsPracticeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWoowahanjsPracticeApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringWoowahanjsPracticeApplication.class);
 	}
 }
